6eebc2b5f687fdeb9d83951a6e98b30d474f71ff
Omar Hernandez <omar.a.hernandez.d@gmail.com>
2022-08-11 08:38:42 -0500

    Stunning Models

--- api/v2/model/UserModel.js
+++ api/v2/model/UserModel.js
@@ -1,13 +1,20 @@
 const mongoose= require("mongoose")
-
+const bcrypt= require("bcryptjs")
 
 const userSchema= new mongoose.Schema({
-    name: 'string',
-    username: 'string',
-    createDate: 'date',
-    updatedDate: 'date',
-    createBy: 'string',
-    updatedBy: 'string',
+    username: { 
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        unique: true
+    },
+    name: String,
+    createDate: Date,
+    updatedDate: Date,
+    createBy: String,
+    updatedBy: String,
 },
 {
     timestamps:{
@@ -16,6 +23,12 @@ const userSchema= new mongoose.Schema({
     }
 }
 )
+
+userSchema.statics.encryptPassword= async (password){
+    const salted= await bcrypt.genSalt(10)
+    return await bcrypt.hash(password,salted)
+}
+
 
 const User= mongoose.model('users',userSchema)
 
